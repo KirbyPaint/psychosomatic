5d4bd778543aa44158f053e538ec3e7322842e08
Ash Porter <ashley.porter@eyecuelab.com>
2022-02-05 13:31:32 -0800

    remove debugging

--- dist/reactions/victoria.js
+++ dist/reactions/victoria.js
@@ -8,16 +8,13 @@ function victoria(msg) {
     // Thanks to grammar, I have to do work
     const modifySentenceStructureWords = [""];
     const leaveItWords = ["you", "me", "we", "they", "she", "he"];
-    console.log("i", i);
-    console.log("think", think);
-    console.log("conditionWord", conditionWord);
-    console.log(leaveItWords.includes(conditionWord));
     if (leaveItWords.includes(conditionWord)) {
-        msg.reply(`DEBUG CONDITION 1 MET: ${conditionWord}`);
         msg.reply(`I THINK WE ALL ${rest.join(" ").toUpperCase()}`);
     }
     else {
-        msg.reply(`I THINK WE ALL THINK ${conditionWord} ${rest.join(" ").toUpperCase()}`);
+        msg.reply(`I THINK WE ALL THINK ${conditionWord.toUpperCase()} ${rest
+            .join(" ")
+            .toUpperCase()}`);
     }
     msg.reply(`https://media.discordapp.net/attachments/799876599372840964/932822173872181278/image0-2.png`);
 }
--- src/reactions/victoria.ts
+++ src/reactions/victoria.ts
@@ -7,16 +7,13 @@ export function victoria(msg: Message) {
   // Thanks to grammar, I have to do work
   const modifySentenceStructureWords = [""];
   const leaveItWords = ["you", "me", "we", "they", "she", "he"];
-  console.log("i", i);
-  console.log("think", think);
-  console.log("conditionWord", conditionWord);
-  console.log(leaveItWords.includes(conditionWord));
   if (leaveItWords.includes(conditionWord)) {
-    msg.reply(`DEBUG CONDITION 1 MET: ${conditionWord}`);
     msg.reply(`I THINK WE ALL ${rest.join(" ").toUpperCase()}`);
   } else {
     msg.reply(
-      `I THINK WE ALL THINK ${conditionWord} ${rest.join(" ").toUpperCase()}`
+      `I THINK WE ALL THINK ${conditionWord.toUpperCase()} ${rest
+        .join(" ")
+        .toUpperCase()}`
     );
   }
   msg.reply(
