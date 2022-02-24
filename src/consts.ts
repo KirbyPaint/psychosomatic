a3db321b94c81b4ba9534318ed5e96679c33310e
Ash Porter <ashley.porter@eyecuelab.com>
2022-02-23 18:50:12 -0800

    add more pics

--- src/consts.ts
+++ src/consts.ts
@@ -4,6 +4,22 @@ export const victoriaReactions = [
   `https://media.discordapp.net/attachments/799876599372840964/943670277865357413/latest.png?width=532&height=686`,
   `https://media.discordapp.net/attachments/799876599372840964/943670518517735434/main-qimg-1b927c7d459676e4366aefee977a76ce-lq.png?width=457&height=686`,
   `https://media.discordapp.net/attachments/799876599372840964/943671162553106482/E8idJuXUUAIt-9K.png`,
+  `https://media.discordapp.com/attachments/799876599372840964/946232922652954684/fto_ft1_262483.jpg`,
+  `https://media.discordapp.com/attachments/799876599372840964/946232922954932234/victoria-justice-kcas-vertical.jpg`,
+  `https://media.discordapp.com/attachments/799876599372840964/946232923223388160/wp7581699.jpg`,
+  `https://media.discordapp.com/attachments/799876599372840964/946232923638607962/victoria-justice-modeliste-magazine-4k_1632431382.jpg`,
+  `https://media.discordapp.com/attachments/799876599372840964/946232924066447390/victoria-justice-secrets-wallpaper-preview.jpg`,
+  `https://media.discordapp.com/attachments/799876599372840964/946232924431323217/aDI0m9.jpg`,
+  `https://media.discordapp.com/attachments/799876599372840964/946232924829790208/wallpapersden.com_victoria-justice-in-black-jacket_3840x2560.jpg`,
+  `https://media.discordapp.com/attachments/799876599372840964/946232925500899358/640768.jpg`,
+  `https://media.discordapp.com/attachments/799876599372840964/946232925999996968/Victoria-Justice-1024x873.jpg`,
+  `https://media.discordapp.com/attachments/799876599372840964/946232926218096680/wallpapersden.com_76955-2560x1440.jpg`,
+  `https://media.discordapp.com/attachments/799876599372840964/946232958438756424/victoria-justice.jpg`,
+  `https://media.discordapp.com/attachments/799876599372840964/946232958841413632/MV5BMTU2NDI2YjktYjYxMy00OGIwLWEzMjktNzEzNzA4YzVmZGRjXkEyXkFqcGdeQXVyNDU4MDk4OA._V1_.jpg`,
+  `https://media.discordapp.com/attachments/799876599372840964/946232959319539722/5L2gbireGntC3SgoL7cyBN8Hp5L.jpg`,
+  `https://media.discordapp.com/attachments/799876599372840964/946232959718011021/sexy-victoria-justice-pictures.jpg`,
+  `https://media.discordapp.com/attachments/799876599372840964/946232960066142258/ab6761610000e5eb48b52d9236cf016e278f3658.jpeg`,
+  `https://media.discordapp.com/attachments/799876599372840964/946232960464609290/victoria_justice_truth.jpg`,
 ];
 
 export function getRandomArbitrary(min: number, max: number): number {
