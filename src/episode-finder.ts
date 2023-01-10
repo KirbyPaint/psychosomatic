ed34344e8e1ecdf862d0ba1e287f86363041562b
Ash Porter <ashley.porter@eyecuelab.com>
2023-01-09 21:30:45 -0800

    fix?

--- src/episode-finder.ts
+++ src/episode-finder.ts
@@ -5,13 +5,13 @@ import { getRandomArbitrary } from "./consts";
 export function whichEpisode(): string {
   const tvDirectory = path.join(process.cwd(), `episodeList.txt`);
   const array = fs.readFileSync(tvDirectory).toString().split(`\n`);
-  console.log({ array });
-  for (const i in array) {
-    const showArray = array[i];
-    console.log({ showArray });
-    const show = showArray[getRandomArbitrary(0, showArray.length - 1)];
-    console.log(`show: ${show}`);
-    return show;
-  }
-  return `no empty messages`;
+  // console.log({ array });
+  // for (const i in array) {
+  // const showArray = array[i];
+  // console.log({ showArray });
+  const show = array[getRandomArbitrary(2, array.length - 1)];
+  console.log(`show: ${show}`);
+  return show;
+  // }
+  // return `no empty messages`;
 }
