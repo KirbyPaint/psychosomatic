e44fd225ff3d06aa868f9cf2c24bc1572328ba7d
Ash Porter <ashley.porter@eyecuelab.com>
2023-01-09 21:31:29 -0800

    ugh

--- src/episode-finder.ts
+++ src/episode-finder.ts
@@ -5,7 +5,7 @@ import { getRandomArbitrary } from "./consts";
 export function whichEpisode(): string {
   const tvDirectory = path.join(process.cwd(), `episodeList.txt`);
   const array = fs.readFileSync(tvDirectory).toString().split(`\n`);
-  // console.log({ array });
+  console.log(array);
   // for (const i in array) {
   // const showArray = array[i];
   // console.log({ showArray });
