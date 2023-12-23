bdf22570ddd5aa64c2f6b252740e89d1062c9ba3
Ash Porter <porter.ashley13@gmail.com>
2023-12-23 12:50:26 -0800

    more 8 balls

--- src/gets/8ball.ts
+++ src/gets/8ball.ts
@@ -23,7 +23,16 @@ const ball = [
   `Very doubtful.`,
   `I don't fucking know.`,
   `It's none of my business.`,
-  `No, I don’t think I will`,
+  `It's none of your business.`,
+  `No, I don’t think I will.`,
+  `Yes, dear.`,
+  `Anyway, here's Wonderwall.`,
+  `Yes, now leave me alone.`,
+  `Kill. Kill. Kill. Kill. Kill. Kill. Kill.`,
+  `Do you think God lives in Heaven because He, too, lives in fear of what He's created here on Earth?`,
+  `Anything is possible through the liberal application of fire.`,
+  `Sure, I mean, it's your funeral.`,
+  `Technically yes, but you'll hate it.`,
 ];
 
 export function get8Ball() {
